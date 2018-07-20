baef55f9559d866ab4dcf434845106739e8b6e44
Tiffany Leigh <tiffanyleigh.10@gmail.com>
2018-07-20 04:42:52 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,37 @@
+def sort_array_asc(array)
+  if a == b
+    0
+  elsif a < b
+    -1
+  elsif a > b
+    i
+  end
+end
 
+def sort_array_desc(array)
+    array = a.sort.reverse
+end
+
+def sort_array_char_count(array)
+    array = array.sort_by {|x| x.length}
+  end
+end
+
+def swap_elements(array)
+    array[2],array[1] = array[1],array[2]
+end
+
+def reverse_array(array, reverse=true)
+    a.sort.reverse
+end
+
+
+def find_a
+    name.select { |name| name[0,1] == A }
+  end
+end
+
+def sum_array
+    [1,2,3].each do |i| puts i
+    end
+end
