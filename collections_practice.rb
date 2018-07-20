c3bf38c9fe44953a61b51219734d828ee24676c1
Tiffany Leigh <tiffanyleigh.10@gmail.com>
2018-07-20 03:17:37 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,45 @@
+def sort_array_asc(array)
+  if a == b
+    0
+  elsif a < b
+    -1
+  elsif a > b
+    i
+  end
+end
 
+def sort_array_desc(array)
+arrayy = a.sort.reverse
+end
+
+def sort_array_char_count(array)
+
+  end
+end
+
+def swap_elements
+
+
+end
+
+def reverse_array(array, reverse=true)
+a.sort.reverse
+end
+
+
+def kesha_maker
+
+end
+
+
+def find_a
+
+end
+
+def sum_array
+
+end
+
+def  add_s(array)
+  array[2],array[1]=array[1],array[2]
+end
